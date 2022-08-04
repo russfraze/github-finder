c814517055a95b41c1732c24165db22a3ab8b329
Russell Fraze <russell.fraze@gmail.com>
2022-08-04 13:19:04 -0700

    fetch users

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
+import Home from './pages/Home'
+import About from './pages/About'
+import NotFound from './pages/NotFound'
 
 function App() {
   return (
@@ -10,7 +13,12 @@ function App() {
         <Navbar />
 
         <main className="container mx-auto px-3 pb-12">
-          Content
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/notfound' element={<NotFound />} />
+            <Route path='/*' element={<NotFound />} />
+          </Routes>
         </main>
 
         <Footer />
